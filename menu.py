18569e9d3c4ecd509a69775d3a7f03cf4d90b8ac
AlanReMx <rechy.martinez.alan@gmail.com>
2025-08-31 21:02:53 -0600

    Add menu function for Simpson methods selection

---
+++ menu.py
@@ -0,0 +1,9 @@
+# crear menu
+def menu():
+    print("Seleccione una opción:")
+    print("1. Método de Simpson 1/3")
+    print("2. Método de Simpson 3/8")
+    print("3. Método de Simpson 4/3")
+    print("4. Salir")
+    opcion = input("Ingrese el número de la opción deseada: ")
+    return opcion
